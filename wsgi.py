37851b16cb0d518af22d463cd68f70320754e2c0
Wajdi Jurry <wajdijurry3@gmail.com>
2025-05-04 16:16:37 +0300

    Fix load env issue

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,9 @@
-from app import create_app
+from dotenv import load_dotenv
 
+# Load environment variables from .env file
+load_dotenv()
+
+from app import create_app
 application = create_app()
 
 if __name__ == '__main__':
